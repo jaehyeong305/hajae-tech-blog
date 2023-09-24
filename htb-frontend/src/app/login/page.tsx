b67697e71085b1444739f011716e81e5b51df079
jaehyeong305 <jaehyeong.ha.305@gmail.com>
2023-09-24 19:38:46 +0900

    HTB-6 add LoginForm component, and modify page component

--- htb-frontend/src/app/login/page.tsx
+++ htb-frontend/src/app/login/page.tsx
@@ -1,7 +1,23 @@
+import Icon from '../components/icon/Icon';
+import LoginForm from './components/LoginForm';
+import styles from './page.module.scss'
+
 const Login: React.FC = () => {
     return (
-        <div>Login Page</div>
-    )
-};
+        <div className={styles.loginPageWrapper}>
+            <div className={styles.loginWrapper}>
+                <span className={styles.loginTitle}> 로 그 인 </span>
+                <LoginForm />
+            </div>
+            {/* <div className={styles.loginInfoWrapper}>
+                <Icon iconName='info' iconCustomStyle={{color: '#FFBB00', marginRight: '10px'}}/>
+                <p>
+                    해당 블로그에서는 방명록 작성을 위한 로그인 기능을 구현했습니다. <br />
+                    <strong>자주 사용하는 이메일과 비밀번호가 아닌 기억만 할 수 있는 정보</strong>를 입력해주세요.
+                </p>
+            </div> */}
+        </div>
+    );
+}
 
 export default Login;
